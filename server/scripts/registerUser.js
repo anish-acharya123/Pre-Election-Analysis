9c8a41f9a01b89b8823d85abc0d918482cca2261
Anish <acharyaanish920@gmail.com>
2024-08-12 17:12:02 +0545

    hiding .env

--- server/scripts/registerUser.js
+++ server/scripts/registerUser.js
@@ -6,7 +6,7 @@ const validateUser = require("../middleware/validateUser");
 const registerUser = async () => {
   try {
     await connectDb();
-    const name = "Anish Acharya";
+    const name = "Bizay Tilimsin";
     const voterId = "789-456-123";
     const citizenshipNumber = "123-456-789";
     const newUser = new ValidateUsers({
